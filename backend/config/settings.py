4972060d4813fcd5ea2508c0698d519aa25f27be
franco <francoemanuel039@gmail.com>
2026-02-09 20:19:06 -0300

    fix: configurar ALLOWED_HOSTS para permitir la IP del servidor

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q*o@@x00e#^ioju^cjcbv3%3ug9w94j$!46xzhuk$+_cmt!w0v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
